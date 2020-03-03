ef52d347ea9fa891a9ae69e40aef03135d96644e
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-03-02 22:58:18 -0300

    Ajeitando o formulário.

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -24,12 +24,12 @@ export class CadastrarPjComponent  implements OnInit {
 
   gerarForm() {
     this.form = this.fb.group({
-      nome: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      senha: ['', [Validators.required, Validators.minLength(6)]],
-      cpf: ['', [Validators.required, ]],
+      nome:        ['', [Validators.required, Validators.minLength(3)]],
+      email:       ['', [Validators.required, Validators.email]],
+      senha:       ['', [Validators.required, Validators.minLength(6)]],
+      cpf:         ['', [Validators.required, ]],
       razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
-      cnpj: ['', [Validators.required ]],
+      cnpj:        ['', [Validators.required ]],
     })
   }
 
